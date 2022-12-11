6a6e19ca08ef5fb4dcf918103c970412cdc66bf8
Vivien DE BONA <vdebona@umanit.fr>
2022-12-11 10:43:18 +0100

    feat: Y2022D10 first part

--- src/Resolvers/Y2022/D10.php
+++ src/Resolvers/Y2022/D10.php
@@ -14,8 +14,6 @@ class D10 implements ResolverInterface
     private int $cycle = 0;
     private int $x = 1;
     private int $signalStrength = 0;
-    private array $pendingInstructions = [];
-    private int $nextInstructionCycle = 0;
     private static array $interestingSignals = [20, 60, 100, 140, 180, 220];
 
     public function resolve(array $input): Solution
@@ -27,43 +25,25 @@ class D10 implements ResolverInterface
 
             ++$this->cycle;
 
-            // $x = $this->x;
+            $this->analyzeSignal();
 
             if (self::NOOP !== $row) {
                 [, $size] = explode(' ', $row);
 
                 $this->addInstruction((int) $size);
             }
-
-            $this->analyzeSignal();
-            $this->processInstructions();
-            // dump('cycle '.$this->cycle.' - begin: '.$x.' - end: '.$this->x);
         }
-
-        $this->processPendingInstructions();
 
         return new Solution($this->signalStrength);
     }
 
     private function addInstruction(int $size): void
     {
-        if (!empty($this->pendingInstructions)) {
-            $executionCycle = max(array_keys($this->pendingInstructions)) + 2;
-        } else {
-            $executionCycle = $this->cycle + 1;
-        }
+        ++$this->cycle;
 
-        $this->pendingInstructions[$executionCycle] = $size;
-    }
+        $this->analyzeSignal();
 
-    private function processInstructions(): void
-    {
-        if (!isset($this->pendingInstructions[$this->cycle])) {
-            return;
-        }
-
-        $this->x += $this->pendingInstructions[$this->cycle];
-        unset($this->pendingInstructions[$this->cycle]);
+        $this->x += $size;
     }
 
     private function analyzeSignal(): void
@@ -72,20 +52,6 @@ class D10 implements ResolverInterface
             return;
         }
 
-        dump($this->cycle.': '.$this->x);
-
         $this->signalStrength += $this->cycle * $this->x;
-    }
-
-    private function processPendingInstructions(): void
-    {
-        $maxCycle = max(array_keys($this->pendingInstructions));
-
-        for ($i = $this->cycle; $i <= $maxCycle; ++$i) {
-            $this->cycle = $i;
-
-            $this->analyzeSignal();
-            $this->processInstructions();
-        }
     }
 }
