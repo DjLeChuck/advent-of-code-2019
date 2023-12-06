e115199a4b8617b6218b448ab6caf96de805e4a4
Vivien De Bona <vdebona@umanit.fr>
2023-12-06 13:31:06 +0100

    feat: Y2023D06 - part 2

--- src/Resolvers/Y2023/D06.php
+++ src/Resolvers/Y2023/D06.php
@@ -18,31 +18,40 @@ class D06 implements ResolverInterface
 
         /** @var array{Time: int[], Distance: int[]} $sheet */
         $sheet = [];
+        /** @var array{Time: int[], Distance: int[]} $sheetTwo */
+        $sheetTwo = [];
 
         foreach ($matches as $match) {
             $key = trim($match[1]);
             $values = array_filter(array_map('\intval', explode(' ', trim($match[2]))));
             $sheet[$key] = [...$values];
+            $sheetTwo[$key] = (int) implode('', $values);
         }
 
         $totalPartOne = [];
 
         foreach ($sheet['Time'] as $round => $duration) {
-            $distance = $sheet['Distance'][$round];
-
-            $beatedRounds = 0;
-
-            for ($x = $duration; $x > 0; $x--) {
-                $newDistance = $x * ($duration - $x);
-
-                if ($newDistance > $distance) {
-                    ++$beatedRounds;
-                }
-            }
-
-            $totalPartOne[] = $beatedRounds;
+            $totalPartOne[] = $this->calculateBeatedRounds($duration, $sheet['Distance'][$round]);
         }
 
-        return new Solution(array_reduce($totalPartOne, static fn(int $carry, int $value) => $carry * $value, 1));
+        return new Solution(
+            array_reduce($totalPartOne, static fn(int $carry, int $value) => $carry * $value, 1),
+            $this->calculateBeatedRounds($sheetTwo['Time'], $sheetTwo['Distance'])
+        );
+    }
+
+    private function calculateBeatedRounds(int $duration, int $distance): int
+    {
+        $beatedRounds = 0;
+
+        for ($x = $duration; $x > 0; $x--) {
+            $newDistance = $x * ($duration - $x);
+
+            if ($newDistance > $distance) {
+                ++$beatedRounds;
+            }
+        }
+
+        return $beatedRounds;
     }
 }
