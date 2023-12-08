d67d146e8a576a80f4d307409f9cceb6e40c30a1
Vivien DE BONA <vdebona@umanit.fr>
2023-12-08 19:57:27 +0100

    Y2023D08 - part 2

--- src/Resolvers/Y2023/D08.php
+++ src/Resolvers/Y2023/D08.php
@@ -10,12 +10,14 @@ use App\Resolvers\ResolverInterface;
 
 class D08 implements ResolverInterface
 {
-    /** @var array<int, Node> */
-    private array $nodes = [];
+    private int $position = -1;
 
     public function resolve(array $input): Solution
     {
+        /** @var array<int, Node> $nodes */
+        $nodes = [];
         $path = str_split(array_shift($input));
+        $partTwoCurrentNodes = [];
 
         foreach ($input as $row) {
             if (empty($row)) {
@@ -23,25 +25,57 @@ class D08 implements ResolverInterface
             }
 
             $matches = [];
-            preg_match('/^([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)$/', $row, $matches);
+            preg_match('/^([A-Z0-9]{3}) = \(([A-Z0-9]{3}), ([A-Z0-9]{3})\)$/', $row, $matches);
 
-            $this->nodes[$matches[1]] = new Node($matches[1], $matches[2], $matches[3]);
+            $nodes[$matches[1]] = new Node($matches[1], $matches[2], $matches[3]);
+
+            if (str_ends_with($matches[1], 'A')) {
+                $partTwoCurrentNodes[] = $nodes[$matches[1]];
+            }
         }
 
+        // Part one
         $nbSteps = 0;
-        $currentNode = $this->nodes['AAA'];
+        $currentNode = $nodes['AAA'];
 
-        do {
-            foreach ($path as $direction) {
-                $currentNode = $this->nodes[$currentNode->getDestinationName($direction)];
-                ++$nbSteps;
+        while (!str_ends_with($currentNode->getName(), 'Z')) {
+            $direction = $this->getDirection($path);
+            $currentNode = $nodes[$currentNode->getDestinationName($direction)];
+            ++$nbSteps;
+        }
 
-                if ('ZZZ' === $currentNode->getName()) {
-                    break 2;
-                }
+        // Part two
+        $steps = array_fill(0, \count($partTwoCurrentNodes), 0);
+
+        foreach ($partTwoCurrentNodes as $i => $currentNode) {
+            while (!str_ends_with($currentNode->getName(), 'Z')) {
+                $direction = $this->getDirection($path);
+                $currentNode = $nodes[$currentNode->getDestinationName($direction)];
+                ++$steps[$i];
             }
-        } while ('ZZZ' !== $currentNode->getName());
+        }
 
-        return new Solution($nbSteps);
+        return new Solution($nbSteps, array_reduce($steps, [$this, 'lcm'], 1));
+    }
+
+    private function getDirection(array $path): string
+    {
+        ++$this->position;
+
+        if ($this->position === \count($path)) {
+            $this->position = 0;
+        }
+
+        return $path[$this->position];
+    }
+
+    private function gcd(float|int $a, float|int $b): float|int
+    {
+        return ($a % $b) ? $this->gcd($b, $a % $b) : $b;
+    }
+
+    private function lcm(float|int $a, float|int $b): float|int
+    {
+        return abs($a * $b) / $this->gcd($a, $b);
     }
 }
