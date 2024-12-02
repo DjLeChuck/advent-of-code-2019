d45080f1c8675db7d4d99f5e3e69e5d54a8149d5
Vivien DE BONA <vdebona@umanit.fr>
2024-12-02 21:08:34 +0100

    feat: add elapsed time

--- solver/2024/d01/d01.go
+++ solver/2024/d01/d01.go
@@ -6,6 +6,7 @@ import (
 	"regexp"
 	"slices"
 	"strconv"
+	"time"
 
 	"github.com/djlechuck/advent-of-code/utils"
 )
@@ -14,8 +15,10 @@ func main() {
 	in := utils.ParseInput("inputs/2024/01.txt")
 	l, r := processInput(in)
 
-	fmt.Printf("Part one: %f\n", partOne(l, r))
-	fmt.Printf("Part two: %d\n", partTwo(l, r))
+	p1v, p1d := partOne(l, r)
+	fmt.Printf("Part one: %.0f - elapsed: %s\n", p1v, p1d)
+	p2v, p2d := partTwo(l, r)
+	fmt.Printf("Part two: %d - elapsed: %s\n", p2v, p2d)
 }
 
 func processInput(in []string) ([]int, []int) {
@@ -49,22 +52,24 @@ func processInput(in []string) ([]int, []int) {
 	return l, r
 }
 
-func partOne(l []int, r []int) float64 {
+func partOne(l []int, r []int) (float64, string) {
+	t := time.Now()
 	d := 0.0
 	for i, vl := range l {
 		vr := r[i]
 
 		d += math.Abs(float64(vl - vr))
 	}
-	return d
+	return d, time.Since(t).String()
 }
 
-func partTwo(l []int, r []int) int {
+func partTwo(l []int, r []int) (int, string) {
+	t := time.Now()
 	d := 0
 	for _, v := range l {
 		d += v * count(r, v)
 	}
-	return d
+	return d, time.Since(t).String()
 }
 
 func count(s []int, v int) int {
