b41c66b873da4af44fd3bb02f5a925fd0532aa85
Vivien DE BONA <vdebona@umanit.fr>
2024-12-09 22:46:43 +0100

    feat(2024d08): add part 2

--- solver/2024/d08/d08.go
+++ solver/2024/d08/d08.go
@@ -18,8 +18,8 @@ func main() {
 
 	p1v, p1d := partOne(g, maxY, maxX)
 	fmt.Printf("Part one: %d - elapsed: %s\n", p1v, p1d)
-	//p2v, p2d := partTwo(g)
-	//fmt.Printf("Part one: %d - elapsed: %s\n", p2v, p2d)
+	p2v, p2d := partTwo(g, maxY, maxX)
+	fmt.Printf("Part one: %d - elapsed: %s\n", p2v, p2d)
 }
 
 func processInput(in utils.Input) (grid, int, int) {
@@ -53,10 +53,10 @@ func partOne(g grid, maxY, maxX int) (int, string) {
 				a := coord{c.x + xDiff, c.y + yDiff}
 				b := coord{c2.x - xDiff, c2.y - yDiff}
 
-				if a.x >= 0 && a.x <= maxX && a.y >= 0 && a.y <= maxY {
+				if inBounds(a, maxY, maxX) {
 					l[a] = nil
 				}
-				if b.x >= 0 && b.x <= maxX && b.y >= 0 && b.y <= maxY {
+				if inBounds(b, maxY, maxX) {
 					l[b] = nil
 				}
 			}
@@ -68,6 +68,53 @@ func partOne(g grid, maxY, maxX int) (int, string) {
 
 func partTwo(g grid, maxY, maxX int) (int, string) {
 	t := time.Now()
+	l := make(map[coord]any)
 
-	return 0, time.Since(t).String()
+	for _, coords := range g {
+		for _, c := range coords {
+			for _, c2 := range coords {
+				if c == c2 {
+					continue
+				}
+
+				anc := getAntinodesCoords(c, c2, maxY, maxX)
+
+				for can := range anc {
+					l[can] = nil
+				}
+			}
+		}
+	}
+
+	return len(l), time.Since(t).String()
+}
+
+func inBounds(c coord, maxY, maxX int) bool {
+	return c.x >= 0 && c.x <= maxX && c.y >= 0 && c.y <= maxY
+}
+
+func getAntinodesCoords(c1, c2 coord, maxY, maxX int) map[coord]any {
+	anc := make(map[coord]any)
+
+	anc[c1] = nil
+	anc[c2] = nil
+
+	xDiff := c1.x - c2.x
+	yDiff := c1.y - c2.y
+	a := coord{c1.x + xDiff, c1.y + yDiff}
+	b := coord{c2.x - xDiff, c2.y - yDiff}
+
+	for inBounds(a, maxY, maxX) {
+		anc[a] = nil
+		a.x += xDiff
+		a.y += yDiff
+	}
+
+	for inBounds(b, maxY, maxX) {
+		anc[b] = nil
+		b.x -= xDiff
+		b.y -= yDiff
+	}
+
+	return anc
 }
